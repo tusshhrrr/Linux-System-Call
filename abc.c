8fec671f31b5162c400c07cd26dda2a9ae6217f4
tusshhrrr <40017223+tusshhrrr@users.noreply.github.com>
2019-09-27 22:26:03 +0530

    Update abc.c

--- abc.c
+++ abc.c
@@ -13,13 +13,16 @@
 #include <linux/file.h>
 #include <linux/errno.h>
 #include <uapi/asm-generic/errno-base.h>
-#include <form.h>
+
 
 asmlinkage long sys_sh_task_info(long pid, char* filename)
 {
     struct task_struct *task;
     FILE *file;
     int a=sys_open(filename, O_WRONLY|O_CREAT,0755);
+    mm_segment_t old_fs = get_fs();
+    set_fs(KERNEL_DS);
+    char buffer[100000];
     file=fopen(filename,"w");
     if (pid<0 || pid>32768)
     {
@@ -32,8 +35,11 @@ asmlinkage long sys_sh_task_info(long pid, char* filename)
             printk("Process : %s\n pid_number : %ld\n process state : %ld\n priority : %ld\n rt_priority : %ld\n static priority : %ld\n normal priority : %ld\n, on_cpu : %ld\n, sched_entity : %u\n, sigset blocked : %d\n,igset real_blocked : %d\n, thread_sp : %lu\n",task->comm, (long)task_pid_nr(task),(long)task->state, (long)task->prio, (long)task->rt_priority, (long)task->static_prio, (long)task->normal_prio, (long)task->on_cpu, (unsigned int)task->se.on_rq,task->blocked,task->real_blocked,task->thread.sp);
             if (a < 0)
                 return EISDIR;
-            fprintf(file,"Process : %s\n pid_number : %ld\n process state : %ld\n priority : %ld\n rt_priority : %ld\n static priority : %ld\n normal priority : %ld\n, on_cpu : %ld\n, sched_entity : %u\n, sigset blocked : %d\n,igset real_blocked : %d\n, thread_sp : %lu\n",task->comm, (long)task_pid_nr(task),(long)task->state, (long)task->prio, (long)task->rt_priority, (long)task->static_prio, (long)task->normal_prio, (long)task->on_cpu, (unsigned int)task->se.on_rq,task->blocked,task->real_blocked,task->thread.sp);
-            fclose(file);
+            sprintf(buffer,file,"Process : %s\n pid_number : %ld\n process state : %ld\n priority : %ld\n rt_priority : %ld\n static priority : %ld\n normal priority : %ld\n, on_cpu : %ld\n, sched_entity : %u\n, sigset blocked : %d\n,igset real_blocked : %d\n, thread_sp : %lu\n",task->comm, (long)task_pid_nr(task),(long)task->state, (long)task->prio, (long)task->rt_priority, (long)task->static_prio, (long)task->normal_prio, (long)task->on_cpu, (unsigned int)task->se.on_rq,task->blocked,task->real_blocked,task->thread.sp);
+            file = fget(fd);
+            file->f_op->write(file, data, strlen(buffer), &file->f_pos);
         }
     }
+    set_fs(old_fs);
+    return 0;
 }
