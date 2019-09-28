0d350f92c522319d2173da30b009531488aedc22
tusshhrrr <40017223+tusshhrrr@users.noreply.github.com>
2019-09-28 18:58:53 +0530

    Update sh_task_info.c

--- sh_task_info.c
+++ sh_task_info.c
@@ -20,9 +20,10 @@ asmlinkage long sys_sh_task_info(long pid, char* filename)
     struct task_struct *task;
     struct file *file;
     int a=sys_open(filename, O_WRONLY|O_CREAT,0755);
+    char buff[100000];
     mm_segment_t old_fs = get_fs();
     set_fs(KERNEL_DS);
-    char buffer[100000];
+
     if (pid<0 || pid>32768)
     {
         return EINVAL;
@@ -30,20 +31,20 @@ asmlinkage long sys_sh_task_info(long pid, char* filename)
     for_each_process(task)
     {
         if ((int) task->pid == pid) {
-            printk("Process : %s\n pid_number : %ld\n process state : %ld\n priority : %ld\n rt_priority : %ld\n static priority : %ld\n normal priority : %ld\n, on_cpu : %ld\n, sched_entity : %u\n, sigset blocked : %d\n,igset real_blocked : %d\n, thread_sp : %lu\n",
-                   task->comm, (long) task_pid_nr(task), (long) task->state, (long) task->prio,
-                   (long) task->rt_priority, (long) task->static_prio, (long) task->normal_prio, (long) task->on_cpu,
-                   (unsigned int) task->se.on_rq, task->blocked, task->real_blocked, task->thread.sp);
+            printk(" Process : %s\n Process PID : %ld\n Process State : %ld\n Process Group : %d\n Session: %d\n Counter: %ld\n Parent Process PID: %d\n Priority : %ld\n normal priority : %ld\n RT_priority : %ld\n static priority : %ld\n on_cpu : %ld\n sched_entity : %u\n thread_sp : %lu\n",
+                   task->comm, (long) task_pid_nr(task), (long) task->state,(int)task->pgrp,(long)task->session,(long)task->counter,(int)task->p_pptr->pid, (long) task->prio,
+                    (long) task->normal_prio,(long) task->rt_priority, (long) task->static_prio, (long) task->on_cpu,
+                   (unsigned int) task->se.on_rq, task->thread.sp);
 
-            sprintf(buffer,
-                    "Process : %s\n pid_number : %ld\n process state : %ld\n priority : %ld\n rt_priority : %ld\n static priority : %ld\n normal priority : %ld\n, on_cpu : %ld\n, sched_entity : %u\n, sigset blocked : %d\n,igset real_blocked : %d\n, thread_sp : %lu\n",
-                    task->comm, (long) task_pid_nr(task), (long) task->state, (long) task->prio,
-                    (long) task->rt_priority, (long) task->static_prio, (long) task->normal_prio, (long) task->on_cpu,
-                    (unsigned int) task->se.on_rq, task->blocked, task->real_blocked, task->thread.sp);
+            sprintf(buff,
+                    " Process : %s\n pid_number : %ld\n process state : %ld\n Process Group : %d\n Session: %d\n Counter: %ld\n Parent Process PID: %d\n priority : %ld\n normal priority : %ld\n rt_priority : %ld\n static priority : %ld\n on_cpu : %ld\n sched_entity : %u\n thread_sp : %lu\n",
+                    task->comm, (long) task_pid_nr(task), (long) task->state,(int)task->pgrp,(long)task->session,(long)task->counter,(int)task->p_pptr->pid, (long) task->prio,
+                     (long) task->normal_prio,(long) task->rt_priority, (long) task->static_prio, (long) task->on_cpu,
+                    (unsigned int) task->se.on_rq, task->thread.sp);
             if (a < 0)
                 return EISDIR;
             file = fget(a);
-            file->f_op->write(file, buffer, strlen(buffer), &file->f_pos);
+            file->f_op->write(file, buff, strlen(buff), &file->f_pos);
 
 
         }
